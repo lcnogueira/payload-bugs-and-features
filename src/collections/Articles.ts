372602fc76aa1166db137ebea90261a64bb9c3aa
lcnogueira <luizcns18@gmail.com>
2026-05-01 14:19:50 -0300

    add article

---
+++ src/collections/Articles.ts
@@ -0,0 +1,49 @@
+import type { CollectionConfig } from 'payload'
+
+export const Articles: CollectionConfig = {
+  slug: 'articles',
+  admin: {
+    useAsTitle: 'title',
+    defaultColumns: ['title', 'createdAt', 'updatedAt'],
+  },
+  versions: {
+    drafts: true,
+  },
+  fields: [
+    {
+      name: 'title',
+      type: 'text',
+      required: true,
+    },
+    {
+      name: 'relatedUser1',
+      type: 'relationship',
+      relationTo: 'users',
+      admin: {
+        appearance: 'drawer',
+      },
+      filterOptions: ({ siblingData }) => {
+        const sibling = siblingData as { relatedUser2?: string }
+        if (sibling?.relatedUser2) {
+          return { id: { not_equals: sibling.relatedUser2 } }
+        }
+        return true
+      },
+    },
+    {
+      name: 'relatedUser2',
+      type: 'relationship',
+      relationTo: 'users',
+      admin: {
+        appearance: 'drawer',
+      },
+      filterOptions: ({ siblingData }) => {
+        const sibling = siblingData as { relatedUser1?: string }
+        if (sibling?.relatedUser1) {
+          return { id: { not_equals: sibling.relatedUser1 } }
+        }
+        return true
+      },
+    },
+  ],
+}
